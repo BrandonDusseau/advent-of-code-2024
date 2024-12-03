2cc6d6b21861f4f02b1197396def434cf4ee42b6
Brandon Dusseau <3901864+BrandonDusseau@users.noreply.github.com>
2024-12-03 10:58:19 -0500

    Clean up 03-2

--- 03-2/main.py
+++ 03-2/main.py
@@ -7,19 +7,17 @@ with open(os.path.join(os.path.dirname(__file__), "input.txt")) as f:
 
 memory = ''.join(lines)
 
-keep = []
+reduced_memory = ""
 # Split memory by don't(). The first segment is always enabled because it's before "don't()".
 # The segment between "don't()" and "do()" (always the first segment when splitting by do())
 # is disabled, so discard it.
 # Recombine all the segments and then scan for mul() commands.
-dont_sections = memory.split("don't()")
-keep.append(dont_sections.pop(0))
-for section in dont_sections:
-    keep = keep + section.split("do()")[1:]
+memory_segments = memory.split("don't()")
+reduced_memory = memory_segments.pop(0)
+for segment in memory_segments:
+    reduced_memory += ''.join(segment.split("do()")[1:])
 
-memory = ''.join(keep)
-
-matches = re.findall(r"mul\((\d+),(\d+)\)", memory)
+matches = re.findall(r"mul\((\d+),(\d+)\)", reduced_memory)
 products = list(map(lambda match:int(match[0]) * int(match[1]), matches))
 
 print(sum(products))

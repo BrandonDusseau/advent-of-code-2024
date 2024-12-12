8fd5bf0ab7187ea85d0e58167feeed2be7980bd1
Brandon Dusseau <3901864+BrandonDusseau@users.noreply.github.com>
2024-12-12 01:53:08 -0500

    Day 11 progress

--- 11-2/main.py
+++ 11-2/main.py
@@ -7,25 +7,60 @@ with open(os.path.join(os.path.dirname(__file__), "input.txt")) as f:
 
 stones = [int(x) for x in line.strip().split(' ')]
 
-stone_memo = {}
 
+def transform_stone(stone):
+    new_stones = []
+    stone_string = str(stone)
+    stone_digits = len(stone_string)
+    if stone == 0:
+        new_stones.append(1)
+    elif stone_digits % 2 == 0:
+        half_length = stone_digits // 2
+        new_stones.append(int(stone_string[0:half_length]))
+        new_stones.append(int(stone_string[half_length:]))
+    else:
+        new_stones.append(stone * 2024)
+
+    return new_stones
+
+
+def cache_stone(stone, layer_cache):
+    stone_set = [stone]
+    for i in range(0, 5):
+        next_set = []
+        for next_stone in stone_set:
+            next_set += transform_stone(next_stone)
+        layer_cache[(stone, i + 1)] = next_set
+        stone_set = next_set
+
+
+def process_stone(stone, current_blink, layer_cache):
+    max_blinks = 75
+
+    if (stone, 1) not in layer_cache:
+        cache_stone(stone, layer_cache)
+    next_set = layer_cache[(stone, 1)]
+
+    if current_blink == max_blinks:
+        return len(next_set)
+
+    sum = 0
+    for next_stone in next_set:
+        if (next_stone, 1) not in layer_cache:
+            cache_stone(next_stone, layer_cache)
+        blinks_left = max_blinks - current_blink
+        blinks_to_skip = min(blinks_left, 5)
+
+        for next_stone_skip in layer_cache[(next_stone, blinks_to_skip - 1)]:
+            sum += process_stone(next_stone_skip, current_blink + blinks_to_skip, layer_cache)
+
+    return sum
+
+
+stone_memo = {}
+layer_cache = {}
 sum = 0
 for this_stone in stones:
-    this_stone_set = [this_stone]
-    for i in range (0, 25):
-        new_stones = []
-        for stone in this_stone_set:
-            stone_string = str(stone)
-            stone_digits = len(stone_string)
-            if stone == 0:
-                new_stones.append(1)
-            elif stone_digits % 2 == 0:
-                half_length = stone_digits // 2
-                new_stones.append(int(stone_string[0:half_length]))
-                new_stones.append(int(stone_string[half_length:]))
-            else:
-                new_stones.append(stone * 2024)
-        this_stone_set = new_stones
-    sum += len(this_stone_set)
+    sum += process_stone(this_stone, 1, layer_cache)
 
 print(sum)

62adccb313fa2c2acc82a95bdb3f5930960a28d2
Brandon Dusseau <3901864+BrandonDusseau@users.noreply.github.com>
2024-12-14 00:31:12 -0500

    Saving a less bad solution for day 12

--- 12-2/main.py
+++ 12-2/main.py
@@ -10,7 +10,7 @@ def is_point_in_region(plant, plots, row, col, max_row, max_col):
 
 def explore_region(discovered, plots, row, col, max_row, max_col):
     if (col, row) in discovered:
-        return (set(), [])
+        return (set(), set())
 
     discovered.add((col, row))
     adjacent_plots_in_region = []
@@ -18,16 +18,17 @@ def explore_region(discovered, plots, row, col, max_row, max_col):
     plant = plots[row][col]
 
     points_to_test = [
-        (col - 1, row),
-        (col + 1, row),
-        (col, row - 1),
-        (col, row + 1)
+        ((col - 1, row), 'vert'),
+        ((col + 1, row), 'vert'),
+        ((col, row - 1), 'horiz'),
+        ((col, row + 1), 'horiz')
     ]
-    for test_col, test_row in points_to_test:
+    for test_point, direction in points_to_test:
+        test_col, test_row = test_point
         if is_point_in_region(plant, plots, test_row, test_col, max_row, max_col):
             adjacent_plots_in_region.append((test_col, test_row))
         else:
-            walls.append((test_col, test_row))
+            walls.append(((test_col, test_row), direction))
 
     plots_in_region = {(col, row)}
     for plot_col, plot_row in adjacent_plots_in_region:
@@ -35,46 +36,45 @@ def explore_region(discovered, plots, row, col, max_row, max_col):
         plots_in_region = plots_in_region.union(found_plots)
         walls += found_walls
 
-    return (plots_in_region, sorted(walls))
+    return (plots_in_region, walls)
 
 
-def count_sides(walls, top_left_point):
-    sides = 0
-    processed_walls = set()
-    walls_to_process = walls.copy()
-    pprint(walls)
+def count_sides(walls):
+    vert_corners = []
+    horiz_corners = []
+    wall_lookup = set(walls)
 
-    current_point = (top_left_point[0], top_left_point[1] - 1)
-    while current_point is not None:
+    for current_point, direction in walls:
         col, row = current_point
-        print(f"Processing point ({col}, {row})")
-        processed_walls.add(current_point)
-        walls_to_process.remove(current_point)
+
+        print(f"Processing point ({col}, {row}) - {direction} wall")
 
         points_to_test = [
             (col - 1, row),
-            (col + 1, row),
-            (col, row - 1),
-            (col, row + 1)
+            (col + 1, row)
         ]
 
-        # Try moving to an adjacent wall.
-        next_point = None
-        for test_point in points_to_test:
-            if test_point in walls and test_point not in processed_walls:
-                print(f"  Found adjacent point at ({test_point[0]}, {test_point[1]})")
-                next_point = test_point
-                break
+        if direction == 'vert':
+            points_to_test = [
+                (col, row - 1),
+                (col, row + 1)
+            ]
 
-        # If there are no adjacent points, pick the next wall that isn't already processed.
-        if next_point is None:
-            sides += 1
-            print(f"  Starting another wall.")
-            next_point = next(iter(walls_to_process)) if len(walls_to_process) > 0 else None
+        if (points_to_test[0], direction) not in wall_lookup:
+            print(f"  Adding point ({points_to_test[0][0]}, {points_to_test[0][1]})")
+            if direction == 'vert':
+                vert_corners.append(points_to_test[0])
+            else:
+                horiz_corners.append(points_to_test[0])
 
-        current_point = next_point
+        if (points_to_test[1], direction) not in wall_lookup:
+            print(f"  Adding point ({points_to_test[1][0]}, {points_to_test[1][1]})")
+            if direction == 'vert':
+                vert_corners.append(points_to_test[1])
+            else:
+                horiz_corners.append(points_to_test[1])
 
-    return sides
+    return max(len(vert_corners), len(horiz_corners))
 
 
 with open(os.path.join(os.path.dirname(__file__), "input.txt")) as f:
@@ -91,20 +91,22 @@ max_row = len(plots) - 1
 max_col = len(plots[0]) - 1
 
 points_assigned_regions = set()
-regions = {}
+regions = []
 for row in range(0, len(plots)):
     for col in range(0, len(plots[row])):
         if (col, row) in points_assigned_regions:
             continue
         region = explore_region(set(), plots, row, col, max_row, max_col)
-        regions[(col, row)] = region
+        regions.append(region)
         points_assigned_regions = points_assigned_regions.union(region[0])
 
+pprint(regions)
+
 total_price = 0
-for top_left_point, region_data in regions.items():
+for region_data in regions:
     region_points, region_walls = region_data
     pprint(region_points)
-    total_sides = count_sides(region_walls, top_left_point)
+    total_sides = count_sides(region_walls)
     print(total_sides)
     print()
     total_price += len(region_points) * total_sides

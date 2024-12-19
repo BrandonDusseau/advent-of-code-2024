9c5bf59b79eae5f21391bd4f4bb99c69ff61faae
Brandon Dusseau <3901864+BrandonDusseau@users.noreply.github.com>
2024-12-19 08:56:26 -0500

    Partial completion of day 19

--- 19-1/main.py
+++ 19-1/main.py
@@ -2,27 +2,30 @@ import os
 from pprint import pprint
 
 
-def design_is_possible(towels, design):
-    print(f"Design {design}")
+def design_is_possible(towels, design, recursion_level=0):
+    print(f"{'  ' * recursion_level}Design {design}")
     start = 0
     end = len(design) - 1
 
-    backtrack_points = []
-    subsets = []
     while start <= end:
-        pprint(backtrack_points)
-        pprint(subsets)
         for temp_end in range(end, start - 1, -1):
             if design[start:temp_end + 1] in towels:
-                print(f"  {design[start:temp_end + 1]} is valid")
-                backtrack_points.append(temp_end)
-                subsets.append(design[start:temp_end + 1])
-                start = temp_end + 1
-                break
+                print(f"{'  ' * recursion_level}{design[start:temp_end + 1]} is valid - recursing with remaining design")
+                if temp_end + 1 > end:
+                    # If the design is valid and we don't have any further design left to check, return true.
+                    return True
+                elif design_is_possible(towels, design[temp_end + 1:end + 1], recursion_level + 1):
+                    # If the remaining design is possible, return true.
+                    return True
+                elif temp_end == start:
+                    print(f"{'  ' * recursion_level}Recursion failed, and we're out of stripes - not possible to continue")
+                    return False
             elif temp_end == start:
-                print("  Not possible to continue")
+                # If the remaining design wasn't possible and there's only one stripe left, the design is not possible.
+                print(f"{'  ' * recursion_level}{design[start]} is NOT valid - Not possible to continue")
                 return False
-            print(f"  {design[start:temp_end + 1]} is NOT valid")
+            else:
+                print(f"{'  ' * recursion_level}{design[start:temp_end + 1]} is NOT valid")
 
     return True
 

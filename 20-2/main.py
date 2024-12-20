0a2c0fbea1d31af3cbd1b9080a25f3fa98430f4b
Brandon Dusseau <3901864+BrandonDusseau@users.noreply.github.com>
2024-12-20 16:39:57 -0500

    Complete day 20

--- 20-2/main.py
+++ 20-2/main.py
@@ -102,35 +102,11 @@ for line_index in range(0, len(lines)):
         if char in {'S', 'E', '.'}:
             spaces.add(point)
 
-# Find which walls will have an effect if they are removed.
-removable_walls = set()
-cheats = set()
-for wall in walls:
-    adjacent_points = get_surrounding_points(wall)
-    adjacent_spaces = [x for x in adjacent_points if x in spaces]
-    adjacent_walls = [x for x in adjacent_points if x in walls]
-
-    # The wall is only useful if it touches at least one open space.
-    if len(adjacent_spaces) >= 1:
-        removable_walls.add(wall)
-
-# Find the shortest distance between any two sets of walls that are adjacent to a track tile.
-# The distance can't exceed 19 (the last step of the cheat has to be onto a track tile), so exclude others.
-cheats = {}
-for wall1 in removable_walls:
-    cheats_for_this_wall = []
-    for wall2 in removable_walls:
-        cheat_cost = abs(wall1[0] - wall2[0]) + abs(wall1[1] - wall2[1]) + 1
-        if cheat_cost <= 19:
-            cheats_for_this_wall.append((wall2, cheat_cost))
-    cheats[wall1] = cheats_for_this_wall
-
 # visualize(spaces, walls, start, end, max_x, max_y)
 
 dist_from_start, prev_data = dijkstra(spaces, start)
 base_time = dist_from_start[end]
 print(f"Base time: {base_time} pS")
-print(f"There are {len(cheats)} cheat entrances")
 
 original_path = []
 original_path_set = set()
@@ -146,33 +122,20 @@ dist_from_end, _ = dijkstra(spaces, end)
 
 saved_times = {}
 points_traversed = set()
-for point in original_path:
-    points_traversed.add(point)
-    # Get cheats adjacent to this point.
-    surrounding_cheats = [x for x in get_surrounding_points(point) if x in cheats]
-    cost_to_this_point = dist_from_start[point]
+for point1 in original_path:
+    points_traversed.add(point1)
 
-    lengths_to_cheat_exits = {}
-    foo = {}
+    time_from_start = dist_from_start[point1]
 
-    for cheat_start in surrounding_cheats:
-        for cheat_end, cheat_distance in cheats[cheat_start]:
-            # Find all the points where this cheat exits onto the base path. Exclude the point we're coming from.
-            exits_on_path = [x for x in get_surrounding_points(cheat_end) if x in original_path_set and x not in points_traversed]
+    for point2 in spaces:
+        cheat_length = abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+        if cheat_length > 20:
+            continue
 
-            for exit in exits_on_path:
-                if exit in lengths_to_cheat_exits and lengths_to_cheat_exits[exit] <= cheat_distance:
-                    # We don't care about other cheats that are longer and have the same exit point.
-                    continue
-                else:
-                    lengths_to_cheat_exits[exit] = cheat_distance
-                    foo[exit] = (cheat_start, cheat_end)
+        time_from_end = dist_from_end[point2]
 
-    for exit, cheat_length in lengths_to_cheat_exits.items():
-        # The calculated distance assumes the first point isn't a movement, so add an extra 1.
-        cost_from_cheat_exit_to_end = dist_from_end[exit] + 1
         # Add the length of the cheat.
-        total_time = cost_to_this_point + cheat_length + cost_from_cheat_exit_to_end
+        total_time = time_from_start + cheat_length + time_from_end
 
         diff = base_time - total_time
 
@@ -184,7 +147,7 @@ for point in original_path:
         else:
             saved_times[diff] += 1
 
-pprint(saved_times)
+# pprint(saved_times)
 
 total_at_least_hundred = 0
 for time_saved, count in saved_times.items():
